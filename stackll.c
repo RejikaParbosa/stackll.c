83bc1456bfce8fec2d619252bc6884bc5a143611
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2024-03-05 12:58:32 +0530

    stackll.c
    stack using linked list

--- stackll.c
+++ stackll.c
@@ -1,6 +1,6 @@
+// Implementation of stack using linked list
 #include<stdio.h>
 #include<stdlib.h>
-#include<malloc.h>
 
 struct node
 {
@@ -11,79 +11,80 @@ struct node
 push()
 {
 	int data;
-	struct node *temp;
-	temp=malloc(sizeof(struct node));
-	printf("Enter the data:");
+	struct node *tmp;
+	tmp=malloc(sizeof(struct node));
+	printf("Enter the data: ");
 	scanf("%d",&data);
-	temp->data=data;
-	temp->next=top;
-	top=temp;
+	tmp->data=data;
+	tmp->next=top;
+	top=tmp;
 }
 
 pop()
 {
-	struct node *temp;
-	temp=malloc(sizeof(struct node));
+	struct node *tmp;
+	tmp=malloc(sizeof(struct node));
 	if(top==NULL)
 	{
-		printf("Stack underflow.\n");
+		printf("\n Stack Underflow");
+		printf("\n");
 		return 0;
 	}
-	temp=top;
-	printf("Element to be popped out is %d\n",temp->data);
-	top=temp->next;
-	free(temp);
-	
+	tmp=top;
+	printf("Element popped out is %d",top->data);
+	top=tmp->next;
+	free(tmp);
+	printf("\n");
 }
-
 
 display()
 {
-		struct node *q;
-        q=malloc(sizeof (struct node));
-        if(top==NULL)
-        {
-                printf("List is empty \n");
-        }
-        q=top;
-        printf("List is:\n");
-        while(q!=NULL)
-        {
-                printf("%d\n",q->data);
-                q=q->next;
-        }
+	struct node *q;
+	q=malloc(sizeof(struct node));
+	if(top==NULL)
+	{
+		printf("List is empty");
+		return;
+	}
+	q=top;
+	printf("List is:\n");
+	while(q!=NULL)
+	{
+		printf("\n %d ", q->data);
+		q=q->next;
+	}
+	printf("\n");
 }
-		
 
-
-
-int main()
+main()
 {
 	int choice;
+	top=NULL;
 	while(1)
 	{
-		printf("1.Push \n");
-		printf("2.Pop \n");
-		printf("3.Display \n");
-		printf("4.Quit \n");
-		printf("Enter your choice:");
+		printf("\n");
+		printf("\tMENU\n");
+		printf("1. Push\n");
+		printf("2. Pop\n");
+		printf("3. Display\n");
+		printf("4. Quit\n");
+		printf("Your choice: ");
 		scanf("%d",&choice);
-                switch(choice)
-                {
-                        case 1:
-                                push();
-                                break;
-                        case 2:
-                                pop();
-                                break;
-                        case 3:
-                                display();
-                                break;
-                        case 4:
-                                exit(1);
-                                break;
-                         default:
-                                printf("Please enter a valid choice");
-                }
-      }
+		switch(choice)
+		{
+			case 1:
+				push();
+				break;
+			case 2:
+				pop();
+				break;
+			case 3:
+				display();
+				break;
+			case 4:
+				exit(1);
+			default:
+				printf("Invalid choice");
+		}
+	}
 }
